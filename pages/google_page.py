00c5f0a823140c16c4fc02a980dc4671372a2186
MIYOSHI Shuji <shuji.miyoshi@sports-aiscience.com>
2024-06-06 20:23:45 +0900

    Elementの取得方法を日本語から変更

--- pages/google_page.py
+++ pages/google_page.py
@@ -3,11 +3,12 @@ from playwright.sync_api import Page
 class GooglePage:
     def __init__(self, page: Page):
         self.page = page
+        self.search_edit = self.page.locator('textarea[name="q"]')
 
     def navigate(self):
         self.page.goto("https://www.google.com")
 
-    def search(self, query: str):
-        self.page.get_by_label("検索", exact=True).click()
-        self.page.get_by_label("検索", exact=True).fill(query)
-        self.page.get_by_label("検索", exact=True).press("Enter")
+    def search(self, query: str):       
+        self.search_edit.click()
+        self.search_edit.fill(query)
+        self.search_edit.press("Enter")
