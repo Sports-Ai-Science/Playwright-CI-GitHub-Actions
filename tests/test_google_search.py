27466121eee55b59bc5c630ea3eb84425027baf5
MIYOSHI Shuji <shuji.miyoshi@sports-aiscience.com>
2024-06-06 20:32:24 +0900

    assert condition changed.

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -6,4 +6,5 @@ def test_google_search(page: Page):
     google_page = GooglePage(page)
     google_page.navigate()
     google_page.search("Playwright Python")
-    assert "Playwright Python - Google 検索" == page.title()
+    
+    assert "https://www.google.com/search?" in page.url
