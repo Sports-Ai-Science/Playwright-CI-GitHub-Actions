aba9b791a83d6a3a436f39af39eb65f9e4ab7c5f
MIYOSHI Shuji <shuji.miyoshi@sports-aiscience.com>
2024-06-27 18:17:55 +0900

    google default page check

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -2,6 +2,13 @@ import pytest
 from playwright.sync_api import Page
 from pages.google_page import GooglePage
 
+def test_google(page: Page):
+    google_page = GooglePage(page)
+    google_page.navigate()
+    
+    assert "https://www.google.com/" == page.url
+
+
 def test_google_search(page: Page):
     google_page = GooglePage(page)
     google_page.navigate()
